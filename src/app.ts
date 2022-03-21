0731a45bf9b6283d40ee7efaeaadfc0350e5f4a3
HeitorFranco <heitorfranco04@gmail.com>
2022-03-21 12:05:21 -0300

    Return formatted yup error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import * as Yup from "yup";
 import { AppError } from "./errors/AppError";
 import { routes } from "./routes/routes";
 
@@ -17,6 +18,12 @@ app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  if (err instanceof Yup.ValidationError) {
+    return response.status(400).json({
       message: err.message,
     });
   }
